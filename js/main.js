4d87e36ecd7318c81ffe419754a8ec80df985176
Tai Luis <tailuis7@gmail.com>
2016-10-12 10:53:37 +0700

    BAckup before fix position 2 circles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-    
+    $(".navbar-default .navbar-collapse .nav > li > a").mouseover(function() {
+        var scrollTop = $(window).scrollTop();
+        var menuOffset = $("nav").offset().top;
+        var distance = (menuOffset - scrollTop);
+        console.log(distance);
+        /*console.log(menuOffset);
+        console.log(scrollTop);*/
+        $(this).siblings(".dropdown-menu").css({
+                "display": "inline-block",
+                "top": distance + 48
+            },
+            "slow");
+        $(this).mouseout(function() {
+            $(this).siblings(".dropdown-menu").css({ "display": "none" }, "slow");
+        });
+        $(this).siblings(".dropdown-menu").mouseover(function() {
+            $(this).css({ "display": "inline-block" }, "slow");
+        });
+    });
+    $(".dropdown-menu").mouseout(function() {
+        $(this).css({ "display": "none" }, "slow");
+    });
 });
